8cf3f697aac3bc31594dc016cb39fccf6b83485b
Tom Sherman <tomsherman159@gmail.com>
2018-06-18 12:46:23 -0400

    added configuration files

---
+++ src/CoGAPS.cpp
@@ -0,0 +1,45 @@
+#include "Rpackage/src/GapsDispatcher.h"
+
+#include <boost/program_options.hpp>
+
+#include <iostream>
+#include <string>
+
+namespace po = boost::program_options;
+
+int main(int argc, char* argv[])
+{
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "produce help message")
+        ("data", po::value<std::string>(), "path to data matrix")
+        ("numPatterns", po::value<unsigned>(), "number of patterns to fit")
+        ("seed", po::value<uint32_t>(), "seed for random number generator")
+        ("outFile", po::value<std::string>(), "name of output file")
+        ("nCores", po::value<unsigned>(), "number of cores to run on")
+    ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm); 
+
+    if (vm.count("help"))
+    {
+        std::cout << desc << "\n";
+        return 0;
+    }
+    else if (!vm.count("data"))
+    {
+        std::cout << "No data file provided\n";
+        return -1;
+    }
+
+    GapsDispatcher dispatcher(vm["seed"].as<uint32_t>());
+    dispatcher.setNumPatterns(vm["numPatterns"].as<unsigned>());
+    dispatcher.setNumCoresPerSet(vm["nCores"].as<unsigned>());
+    dispatcher.loadData(vm["data"].as<std::string>());
+    GapsResult result(dispatcher.run());
+    result.writeCsv(vm["outFile"].as<std::string>());
+
+    return 0;
+}
