fe35d08e198ed7b3f54555e823bd096b3d89d602
Tom Sherman <tomsherman159@gmail.com>
2018-06-21 18:00:49 -0400

    dont use boost for command line arguments

--- src/CoGAPS.cpp
+++ src/CoGAPS.cpp
@@ -1,47 +1,102 @@
 #include "Rpackage/src/GapsDispatcher.h"
 
-#include <boost/program_options.hpp>
-
-#include <iostream>
 #include <string>
 
-namespace po = boost::program_options;
+#include <cstdlib>
+#include <cstdio>
+#include <getopt.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char** argv)
 {
-    po::options_description desc("Allowed options");
-    desc.add_options()
-        ("help", "produce help message")
-        ("data", po::value<std::string>(), "path to data matrix")
-        ("nPatterns", po::value<unsigned>()->default_value(7), "number of patterns to fit")
-        ("nIterations", po::value<unsigned>()->default_value(1000), "number of iterations to run cogaps for")
-        ("seed", po::value<uint32_t>()->default_value(0), "seed for random number generator")
-        ("outFile", po::value<std::string>()->default_value("cogaps_result"), "name of output file")
-        ("nCores", po::value<unsigned>()->default_value(1), "number of cores to run on")
-    ;
+    // internal parameters    
+    std::string data;
+    unsigned nPatterns = 7;
+    unsigned nIterations = 1000;
+    unsigned nCores = 1;
+    unsigned seed = 0;
+    std::string outFile("cogaps_result");
 
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm); 
-
-    if (vm.count("help"))
+    // names of allowed parameters
+    const char *short_opt = "d:p:i:c:s:o:h";
+    struct option long_opt[] =
     {
-        std::cout << desc << "\n";
-        return 0;
+        {"data",          required_argument, NULL, 'd'},
+        {"nPatterns",     required_argument, NULL, 'p'},
+        {"nIterations",   required_argument, NULL, 'i'},
+        {"nCores",        required_argument, NULL, 'c'},
+        {"seed",          required_argument, NULL, 's'},
+        {"outFile",       required_argument, NULL, 'o'},
+        {"help",          no_argument,       NULL, 'h'},
+        {NULL,            0,                 NULL,  0 }
+    };
+
+    // parse command line
+    int c = 0;
+    while ((c = getopt_long(argc, argv, short_opt, long_opt, NULL)) != -1)
+    {
+        switch(c)
+        {
+            case -1: // no more arguments
+            case 0:  // long options toggles
+                break;
+            case 'd':
+                data = optarg;
+                break;
+            case 'p':
+                nPatterns = atoi(optarg);
+                break;
+            case 'i':
+                nIterations = atoi(optarg);
+                break;
+            case 'c':
+                nCores = atoi(optarg);
+                break;
+            case 's':
+                seed = atoi(optarg);
+                break;
+            case 'o':
+                outFile = optarg;
+                break;
+            case 'h':
+                printf("Usage: %s [OPTIONS]\n", argv[0]);
+                printf("  -d, --data file         count matrix file\n");
+                printf("  -p, --nPatterns N       number of patterns\n");
+                printf("  -i, --nIterations N     max number of iterations\n");
+                printf("  -c, --nCores N          max number of cores to use\n");
+                printf("  -s, --seed N            random generator seed\n");
+                printf("  -o, --output file       output file name\n");
+                printf("  -h, --help              print this help message\n");
+                printf("\n");
+                return 0;
+
+            case ':':
+            case '?':
+                fprintf(stderr, "Try `%s --help' for more information.\n", argv[0]);
+                return -1;
+
+            default:
+                fprintf(stderr, "%s: invalid option -- %c\n", argv[0], c);
+                fprintf(stderr, "Try `%s --help' for more information.\n", argv[0]);
+                return -1;
+        };
     }
-    else if (!vm.count("data"))
+
+    // required parameters
+    if (data.empty())
     {
-        std::cout << "No data file provided\n";
+        fprintf(stderr, "no data file provided\n");
+        fprintf(stderr, "Try `%s --help' for more information.\n", argv[0]);
         return -1;
     }
 
-    GapsDispatcher dispatcher(vm["seed"].as<uint32_t>());
-    dispatcher.setNumPatterns(vm["nPatterns"].as<unsigned>());
-    dispatcher.setMaxIterations(vm["nIterations"].as<unsigned>());
-    dispatcher.setNumCoresPerSet(vm["nCores"].as<unsigned>());
-    dispatcher.loadData(vm["data"].as<std::string>());
+    // run cogaps
+    GapsDispatcher dispatcher(seed);
+    dispatcher.setNumPatterns(nPatterns);
+    dispatcher.setMaxIterations(nIterations);
+    dispatcher.setNumCoresPerSet(nCores);
+    dispatcher.loadData(data);
     GapsResult result(dispatcher.run());
-    //result.writeCsv(vm["outFile"].as<std::string>());
+    //result.writeCsv(outFile);
 
     return 0;
 }
